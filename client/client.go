7d4ad47ed45cfc3795ff6b77b980c17073959e3b
Roberto Santalla <roobre@roobre.es>
2022-06-09 15:52:08 +0200

    client: set stricter timeouts for dial, tls, and response headers

--- client/client.go
+++ client/client.go
@@ -3,6 +3,7 @@ package client
 import (
 	"fmt"
 	log "github.com/sirupsen/logrus"
+	"net"
 	"net/http"
 	"strings"
 	"time"
@@ -20,14 +21,33 @@ type Request struct {
 
 type Response struct {
 	HTTPResponse *http.Response
+	Worker       string
 	Error        error
-	Done         func()
+	Done         func(written int64)
 }
 
 func NewClient(baseUrl string) *Client {
+	// TODO: Make all these timeouts configurable
+	// Ref: https://blog.cloudflare.com/content/images/2016/06/Timeouts-002.png
+	dialer := &net.Dialer{
+		Timeout:  2 * time.Second,
+		Resolver: &net.Resolver{},
+	}
+	dialer.Resolver.Dial = dialer.DialContext
+
+	transport := &http.Transport{
+		Proxy:                 http.ProxyFromEnvironment,
+		DialContext:           dialer.DialContext,
+		MaxIdleConns:          10,
+		ResponseHeaderTimeout: 2 * time.Second,
+		IdleConnTimeout:       2 * time.Second,
+		TLSHandshakeTimeout:   2 * time.Second,
+	}
+
 	return &Client{
 		HTTPClient: &http.Client{
-			Timeout: 2 * time.Minute,
+			Transport: transport,
+			Timeout:   2 * time.Minute,
 		},
 		baseUrl: baseUrl,
 	}
@@ -37,14 +57,21 @@ func (c *Client) String() string {
 	return c.baseUrl
 }
 
+func (c *Client) URL(path string) string {
+	url := strings.TrimSuffix(c.baseUrl, "/")
+	url += "/"
+	url += strings.TrimPrefix(path, "/")
+
+	return url
+}
+
 func (c *Client) Do(path string) (r Response) {
 	// TODO: Calculate a better deadline by making a HEAD request and a target throughput
 	//ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(2*time.Second))
 	//defer cancel()
 
-	url := strings.TrimSuffix(c.baseUrl, "/")
-	url += "/"
-	url += strings.TrimPrefix(path, "/")
+	url := c.URL(path)
+
 	//req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
@@ -52,7 +79,7 @@ func (c *Client) Do(path string) (r Response) {
 		return
 	}
 
-	log.Infof("%s %s", req.Method, req.URL.String())
+	log.Debugf("%s %s", req.Method, req.URL.String())
 	resp, err := c.HTTPClient.Do(req)
 	if err != nil {
 		r.Error = fmt.Errorf("performing %s to %q: %w", req.Method, req.URL.String(), err)
